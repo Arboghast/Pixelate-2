4deb7a85f07a6ce0d25ca4508d38280ce0b58890
Sami <BlueBubbleBeam@outlook.com>
2019-07-26 16:53:50 -0400

    Updates

---
+++ precache-manifest.a2e3662f6f986c73d31bb3b682c08021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e22aae1e1a06741a4018630a4f43dd",
+    "url": "/Pixelate-2/index.html"
+  },
+  {
+    "revision": "faf5346dddd792661d1a",
+    "url": "/Pixelate-2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Pixelate-2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "faf5346dddd792661d1a",
+    "url": "/Pixelate-2/static/js/main.0bab18d0.chunk.js"
+  },
+  {
+    "revision": "077b17ddea6db2c3d016",
+    "url": "/Pixelate-2/static/js/runtime~main.f0421391.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pixelate-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
